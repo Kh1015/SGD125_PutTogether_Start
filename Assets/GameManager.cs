1e103c0c3f86ad113123103027c190e9ca8571d1
kh1015 <khastings1007@gmail.com>
2023-09-23 00:15:33 -0400

    Final Project Finished

---
+++ Assets/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gm;
+    
+
+    void Awake()
+    {
+        if (!gm)
+            gm = this;
+        else
+            Destroy(this);
+    }
+
+    public GameObject player;
+    public Transform[] nodes;
+
+}
